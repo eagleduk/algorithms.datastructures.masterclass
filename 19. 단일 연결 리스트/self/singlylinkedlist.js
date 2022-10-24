f9ba6f68cc6822b9970617268b6401b02c42d678
SJ.Lee <eagleduk@gmail.com>
2022-10-24 23:59:25 +0900

    114. 단일 연결 리스트 : pop 메소드 솔루션

--- 19. 단일 연결 리스트/self/singlylinkedlist.js
+++ 19. 단일 연결 리스트/self/singlylinkedlist.js
@@ -11,6 +11,7 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  // 새로운 값을 마지막에 추가한다.
   push(val) {
     var node = new Node(val);
     if (this.length === 0) {
@@ -20,8 +21,27 @@ class SinglyLinkedList {
       this.tail.next = node;
       this.tail = node;
     }
-
     this.length += 1;
+    return this;
+  }
+  // 제일 마지막 값의 연결을 제거하고 출력한다.
+  pop() {
+    if (this.length === 0) return undefined;
+    var pre = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return pre;
+    }
+    while (pre.next.next) {
+      pre = pre.next;
+    }
+    var last = pre.next;
+    pre.next = null;
+    this.tail = pre;
+    this.length--;
+    return last;
   }
 }
 
@@ -29,5 +49,6 @@ var list = new SinglyLinkedList();
 
 list.push("Hi");
 list.push("Hello");
+list.push("how");
 
 console.log(list);
