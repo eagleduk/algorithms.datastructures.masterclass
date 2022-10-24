6eb4500054c26afb53cadc9c579ad5f19998c0a8
SJ.Lee <eagleduk@gmail.com>
2022-10-24 23:20:15 +0900

    111. 코드 스타터와 push 메소드 소개

---
+++ 19. 단일 연결 리스트/self/singlylinkedlist.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var node = new Node(val);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+
+    this.length += 1;
+  }
+}
+
+var list = new SinglyLinkedList();
+
+list.push("Hi");
+list.push("Hello");
+
+console.log(list);
