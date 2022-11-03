115a684ad17885e99cab64a8dd799ce8de7a4fc7
SJ.Lee <eagleduk@gmail.com>
2022-11-04 00:06:20 +0900

    188. 힙: ExtractMax 메소드 솔루션

--- 24. 이진 힙/self/heap.js
+++ 24. 이진 힙/self/heap.js
@@ -7,18 +7,57 @@ class MaxBinaryHeap {
     this.values.push(value);
 
     // 부모 노드와 비교
-    var index = this.values.length - 1;
+    let index = this.values.length - 1;
     while (index > 0) {
-      var pIndex = Math.floor((index - 1) / 2);
+      let pIndex = Math.floor((index - 1) / 2);
 
       if (this.values[index] <= this.values[pIndex]) break;
 
-      var temp = this.values[pIndex];
+      let temp = this.values[pIndex];
       this.values[pIndex] = this.values[index];
       this.values[index] = temp;
       index = pIndex;
     }
     return this.values;
+  }
+  remove() {
+    if (!this.value.length) return undefined;
+    // root 제거
+    let result = this.values[0];
+    // 마지막 값을 root로 이동
+    this.values[0] = this.values.pop();
+
+    let idx = 0;
+    while (true) {
+      let lChildIdx = idx * 2 + 1;
+      let rChildIdx = idx * 2 + 2;
+
+      let value = this.values[idx];
+      let lValue = this.values[lChildIdx];
+      let rValue = this.values[rChildIdx];
+
+      let temp;
+      if (value < lValue && value < rValue) {
+        if (lValue < rValue) {
+          this.values[rChildIdx] = value;
+          this.values[idx] = rValue;
+          idx = rChildIdx;
+        } else {
+          this.values[lChildIdx] = value;
+          this.values[idx] = lValue;
+          idx = lChildIdx;
+        }
+      } else if (value < lValue) {
+        this.values[lChildIdx] = value;
+        this.values[idx] = lValue;
+        idx = lChildIdx;
+      } else if (value < rValue) {
+        this.values[rChildIdx] = value;
+        this.values[idx] = rValue;
+        idx = rChildIdx;
+      } else break;
+    }
+    return result;
   }
 }
 
