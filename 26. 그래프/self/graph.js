a25985dad3e0f3ab97d5c9acb0fe721aa89fa8f8
SJ.Lee <eagleduk@gmail.com>
2022-11-08 22:28:41 +0900

    218. 선(Edge) 제거 솔루션

--- 26. 그래프/self/graph.js
+++ 26. 그래프/self/graph.js
@@ -11,9 +11,22 @@ class Graph {
       this.adjacencyList[vertex2].push(vertex1);
     }
   }
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (vertex) => vertex !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (vertex) => vertex !== vertex1
+      );
+    }
+  }
 }
 
 let g = new Graph();
 g.addVertex("Dallas");
 g.addVertex("Tokyo");
 g.addVertex("Aspen");
+
+g.addEgde("Dallas", "Tokyo");
+g.addEgde("Dallas", "Aspen");
