5811e6d098a7d847d09fd171628ba50481f58d34
SJ.Lee <eagleduk@gmail.com>
2022-11-08 22:22:08 +0900

    216. 점(Edge) 추가 솔루션

--- 26. 그래프/self/graph.js
+++ 26. 그래프/self/graph.js
@@ -5,4 +5,15 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+  addEgde(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
 }
+
+let g = new Graph();
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
