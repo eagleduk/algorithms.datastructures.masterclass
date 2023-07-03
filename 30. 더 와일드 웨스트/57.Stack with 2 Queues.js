d02cfcec8d2205270603bf9838ea82830b2b59e9
SJ.Lee <eagleduk@gmail.com>
2023-07-03 10:45:06 +0900

    코딩연습-4

---
+++ 30. 더 와일드 웨스트/57.Stack with 2 Queues.js
@@ -0,0 +1,55 @@
+class Stack {
+  constructor() {
+    this.queue = new Queue();
+  }
+  push(val) {
+    this.queue.enqueue(val);
+    return this;
+  }
+  pop() {
+    var value = this.queue.dequeue();
+    return value;
+  }
+}
+
+// QUEUE AND NODE HAVE BEEN IMPLEMENTED FOR YOU
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(data) {
+    var node = new Node(data);
+
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      node.next = this.first;
+      this.first = node;
+    }
+
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+
+    var temp = this.first;
+    if (this.first == this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
